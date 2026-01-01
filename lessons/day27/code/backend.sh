f389de8ef9354910296fbfe93c5662254d7387d0
Yash Chavan <yashchavan4628@gmail.com>
2026-01-01 15:08:50 +0000

    Fix Policy IAM

--- lessons/day27/code/backend.sh
+++ lessons/day27/code/backend.sh
@@ -5,6 +5,7 @@ aws s3 mb s3://yash-chavan-web-new-bucket-day-27 --region us-east-1
 aws s3api put-bucket-versioning \
   --bucket yash-chavan-web-new-bucket-day-27 \
   --versioning-configuration Status=Enabled
+  
 
 # Enable encryption
 aws s3api put-bucket-encryption \
