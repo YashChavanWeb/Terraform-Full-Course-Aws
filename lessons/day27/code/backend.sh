1dc21f8219746a680ee15d438e10c45cc24db39e
Yash Chavan <yashchavan4628@gmail.com>
2026-01-01 15:00:05 +0000

    Day 27: Fix

---
+++ lessons/day27/code/backend.sh
@@ -0,0 +1,18 @@
+# Create bucket for Terraform state
+aws s3 mb s3://yash-chavan-web-new-bucket-day-27 --region us-east-1
+
+# Enable versioning for state history
+aws s3api put-bucket-versioning \
+  --bucket yash-chavan-web-new-bucket-day-27 \
+  --versioning-configuration Status=Enabled
+
+# Enable encryption
+aws s3api put-bucket-encryption \
+  --bucket yash-chavan-web-new-bucket-day-27 \
+  --server-side-encryption-configuration '{
+    "Rules": [{
+      "ApplyServerSideEncryptionByDefault": {
+        "SSEAlgorithm": "AES256"
+      }
+    }]
+  }'
